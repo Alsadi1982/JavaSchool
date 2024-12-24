51588a58e52e7608fe97793f841df5b0ba36ed32
Alexandr Sadikov <sav1982@inbox.ru>
2024-12-24 16:23:48 +0300

    Реализация продюсера Задание 1

---
+++ src/main/java/sbp/school/kafka/service/TransactionService.java
@@ -0,0 +1,43 @@
+package sbp.school.kafka.service;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import sbp.school.kafka.entity.TransactionEntity;
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Класс реализует основную логику отправки сообщений
+ */
+public class TransactionService {
+
+    private static final Logger LOGGER = Logger.getLogger(TransactionService.class.getName());
+    private final Properties props;
+
+    public TransactionService(Properties props) {
+        this.props = props;
+    }
+
+    /**
+     * Метод TransactionService#send(ProducerRecord<String, TransactionEntity> record) отправка сообщения
+     * @param record
+     */
+    public void send(ProducerRecord<String, TransactionEntity> record){
+        KafkaProducer<String, TransactionEntity> producer = new KafkaProducer<>(props);
+        Future<RecordMetadata> future = producer.send(record, ((metadata, exception) -> {
+            if (exception != null) {
+                String errorMessage = String.format("Сбой передачи сообщения! offset = %d, partition = %d, Exception: %s",
+                                                metadata.offset(), metadata.partition(), exception.getMessage());
+                LOGGER.log(Level.WARNING, errorMessage, exception);
+            } else {
+                String successMessage = String.format("Успешная отправка сообщения! offset = %d, partition = %d, topic = %s",
+                                                metadata.offset(), metadata.partition(), metadata.topic());
+                LOGGER.info(successMessage);
+            }
+        }));
+        producer.flush();
+    }
+}
