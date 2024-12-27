fc63186d546f25a4926e707cc1fbc191a4e6848d
Alexandr Sadikov <sav1982@inbox.ru>
2024-12-28 01:42:33 +0300

    Если сервис упал, отправляю сообщения, закрываю продюсер

--- src/main/java/sbp/school/kafka/service/TransactionService.java
+++ src/main/java/sbp/school/kafka/service/TransactionService.java
@@ -26,18 +26,28 @@ public class TransactionService {
      * @param record
      */
     public void send(ProducerRecord<String, TransactionEntity> record){
-        KafkaProducer<String, TransactionEntity> producer = new KafkaProducer<>(props);
-        Future<RecordMetadata> future = producer.send(record, ((metadata, exception) -> {
-            if (exception != null) {
-                String errorMessage = String.format("Сбой передачи сообщения! offset = %d, partition = %d, Exception: %s",
-                                                metadata.offset(), metadata.partition(), exception.getMessage());
-                LOGGER.log(Level.WARNING, errorMessage, exception);
-            } else {
-                String successMessage = String.format("Успешная отправка сообщения! offset = %d, partition = %d, topic = %s",
-                                                metadata.offset(), metadata.partition(), metadata.topic());
-                LOGGER.info(successMessage);
+        KafkaProducer<String, TransactionEntity> producer = null;
+        try  {
+            producer = new KafkaProducer<>(props);
+            Future<RecordMetadata> future = producer.send(record, ((metadata, exception) -> {
+                if (exception != null) {
+                    String errorMessage = String.format("Сбой передачи сообщения! offset = %d, partition = %d, Exception: %s",
+                            metadata.offset(), metadata.partition(), exception.getMessage());
+                    LOGGER.log(Level.WARNING, errorMessage, exception);
+                } else {
+                    String successMessage = String.format("Успешная отправка сообщения! offset = %d, partition = %d, topic = %s",
+                            metadata.offset(), metadata.partition(), metadata.topic());
+                    LOGGER.info(successMessage);
+                }
+            }));
+        } catch(Exception ex) {
+            LOGGER.log(Level.WARNING, "Что-тo пошло не так! Сервис упал!", ex.getMessage());
+        } finally {
+            if (producer != null) {
+                producer.flush();
+                producer.close();
             }
-        }));
-        producer.flush();
+        }
+
     }
 }
