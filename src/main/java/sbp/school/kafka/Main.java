1f96eb3c2410ef777df7c8ed22eaede44062cb9b
Alexandr Sadikov <sav1982@inbox.ru>
2025-01-23 22:32:47 +0300

    Задание 3: Реализация слушателя обратного потока

--- src/main/java/sbp/school/kafka/Main.java
+++ src/main/java/sbp/school/kafka/Main.java
@@ -1,7 +1,19 @@
 package sbp.school.kafka;
 
+import sbp.school.kafka.config.BackFlowKafkaConfig;
+import sbp.school.kafka.config.LoggerConfig;
+import sbp.school.kafka.service.ThreadListener;
+
+import java.util.concurrent.*;
+
+
 public class Main {
-    public static void main(String[] args) {
+    static {
+        LoggerConfig.getLoggerConfig();
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.submit(new ThreadListener(BackFlowKafkaConfig.getKafkaProperties().getProperty("kafka.hashSum.topic.name"))).get();
 
     }
 }
---
+++ src/main/java/sbp/school/kafka/service/ThreadListener.java
@@ -0,0 +1,52 @@
+package sbp.school.kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sbp.school.kafka.config.BackFlowKafkaConfig;
+import sbp.school.kafka.config.KafkaConfig;
+import sbp.school.kafka.dao.BackFlowJDBCService;
+import sbp.school.kafka.entity.HashSumDto;
+import sbp.school.kafka.entity.TransactionEntity;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ThreadListener extends Thread{
+
+    private static final Logger log = LoggerFactory.getLogger(ThreadListener.class);
+    private final TransactionService transactionService;
+    private final BackFlowJDBCService JDBCService;
+    private final BackFlowConsumerService backFlowConsumerService;
+    private final String kafkaTopic;
+
+    public ThreadListener(String kafkaTopic) {
+        this.backFlowConsumerService = new BackFlowConsumerService(BackFlowKafkaConfig.getKafkaProperties());
+        this.JDBCService = new BackFlowJDBCService();
+        this.kafkaTopic = kafkaTopic;
+        this.transactionService = new TransactionService(KafkaConfig.getKafkaProperties());
+    }
+
+    public void listen() {
+        HashSumDto hashSum = backFlowConsumerService.read((kafkaTopic));
+        long hahSumFromTopic = hashSum.getHashSum();
+        List<TransactionEntity> transactionList = JDBCService.getListByPeriod(hashSum.getFromDate());
+        long hashSumFromDB = transactionList.stream()
+                .map(TransactionEntity::getId)
+                .reduce(0, Integer::sum);
+        log.info("hahSumFromTopic = {}, hashSumFromDB = {}, equals = {}", hahSumFromTopic, hashSumFromDB, Objects.equals(hahSumFromTopic, hashSumFromDB));
+        if (hahSumFromTopic !=  hashSumFromDB) {
+            for (TransactionEntity transaction : transactionList) {
+                transactionService.reSend(transaction, BackFlowKafkaConfig.getKafkaProperties().getProperty("kafka.producer.topic.name"));
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+       listen();
+    }
+}
